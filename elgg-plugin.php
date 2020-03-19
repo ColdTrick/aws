2a8e7f5ebd4f092d3e1e0b820ac2d9fb9765d3ae
Jerome Bakker <jeabakker@coldtrick.com>
2020-03-19 14:51:21 +0100

    chore: moved start.php to elgg-plugin.php

---
+++ elgg-plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(__DIR__ . '/lib/functions.php');
+
+return [
+	'actions' => [
+		'aws/admin/reset_upload' => [
+			'access' => 'admin',
+		],
+	],
+	'events' => [
+		'delete' => [
+			'object' => [
+				'ColdTrick\AWS\Events::deleteObject' => [],
+			],
+		],
+	],
+	'hooks' => [
+		'cron' => [
+			'minute' => [
+				'ColdTrick\AWS\Cron::cleanupS3' => [],
+				'ColdTrick\AWS\Cron::uploadFilesToS3' => [],
+			],
+		],
+		'upload:subtypes' => [
+			'aws:s3' => [
+				'ColdTrick\AWS\Plugins\File::registerSubtypesForUpload' => [],
+			],
+		],
+	],
+];
